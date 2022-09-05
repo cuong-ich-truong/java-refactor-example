75fe068cc6aafe0163f92ea044baa4539e4712bd
cuong-ich-truong <cuongichtruong@gmail.com>
2022-09-05 16:37:50 +0700

    step1: extract methods

--- example/src/main/java/com/example/refactor/Payment.java
+++ example/src/main/java/com/example/refactor/Payment.java
@@ -21,114 +21,132 @@ public class Payment {
             PaymentMethod payMethod) {
         Invoice invoice = null;
 
-        if (payMethod == PaymentMethod.CREDIT_CARD) {
-            CreditCard creditCard = new CreditCard(cardNumber, cardDate, cardCvv);
-            boolean result = creditCard.deductAmount(amount);
-            invoice = new Invoice(customerName, amount, creditCard.getLastForDigits(),
-                    LocalDateTime.now().toString());
-            invoice.setIsPaid(result);
-            String historyRecord = "Payment: " + "Customer " + invoice.getCustomerName()
-                    + " has made a payment with amount="
-                    + invoice.getAmount() + " on " + invoice.getPaymentDate() + " using credit card" + "\r\n";
-            System.out.print(historyRecord);
-            try {
-                File historyFile = new File("paymentLog.txt");
-                if (!historyFile.exists()) {
-                    historyFile.createNewFile();
-                }
-                Files.write(Paths.get("paymentLog.txt"), historyRecord.getBytes(), StandardOpenOption.APPEND);
-                if (invoice.isPaid()) {
-                    String statusMessage = "Payment: " + "Invoice " + invoice.Number() + " - Collected amount of "
-                            + invoice.getAmount()
-                            + "\r\n";
-                    System.out.print(statusMessage);
-                    Files.write(Paths.get("paymentLog.txt"), statusMessage.getBytes(), StandardOpenOption.APPEND);
-                } else {
-                    String statusMessage = "Payment: " + "Invoice " + invoice.Number()
-                            + " - Cannot collect amount of " + invoice.getAmount()
-                            + "\r\n";
-                    System.out.print(statusMessage);
-                    Files.write(Paths.get("paymentLog.txt"), statusMessage.getBytes(), StandardOpenOption.APPEND);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+        switch (payMethod) {
+            case CREDIT_CARD:
+                invoice = payByCreditCard(amount, customerName, cardNumber, cardDate, cardCvv);
+                return invoice;
+            case PAYPAL:
+                invoice = payByPayPal(amount, customerName, customerEmail, paypalPassword);
+                return invoice;
+            case APPLE_PAY:
+                invoice = payByApplePay(amount, customerName, customerEmail, appleId, applePayPassword);
+                return invoice;
+            default:
+                throw new IllegalArgumentException("Unsupported payment method");
+        }
+
+    }
+
+    private Invoice payByApplePay(BigDecimal amount, String customerName, String customerEmail, String appleId,
+            String applePayPassword) {
+        Invoice invoice;
+        ApplePayAccount applePayAccount = new ApplePayAccount(customerEmail, appleId);
+        applePayAccount.signIn(applePayPassword);
+        boolean result = applePayAccount.deductAmount(amount);
+        invoice = new Invoice(customerName, customerEmail, appleId, amount,
+                LocalDateTime.now().toString());
+        invoice.setIsPaid(result);
+
+        String historyRecord = "Payment: " + "Customer " + invoice.getCustomerName()
+                + " has made a payment with amount="
+                + invoice.getAmount() + " on " + invoice.getPaymentDate() + " using ApplePay" + "\r\n";
+        System.out.print(historyRecord);
+        try {
+            File historyFile = new File("paymentLog.txt");
+            if (!historyFile.exists()) {
+                historyFile.createNewFile();
             }
-
-            return invoice;
-
-        } else if (payMethod == PaymentMethod.PAYPAL) {
-            PayPalAccount payPalAccount = new PayPalAccount(customerEmail);
-            payPalAccount.signIn(paypalPassword);
-            boolean result = payPalAccount.deductAmount(amount);
-            invoice = new Invoice(customerName, customerEmail, amount,
-                    LocalDateTime.now().toString());
-            invoice.setIsPaid(result);
-
-            String historyRecord = "Payment: " + "Customer " + invoice.getCustomerName()
-                    + " has made a payment with amount="
-                    + invoice.getAmount() + " on " + invoice.getPaymentDate() + " using Paypal" + "\r\n";
-            System.out.print(historyRecord);
-            try {
-                File historyFile = new File("paymentLog.txt");
-                if (!historyFile.exists()) {
-                    historyFile.createNewFile();
-                }
-                Files.write(Paths.get("paymentLog.txt"), historyRecord.getBytes(), StandardOpenOption.APPEND);
-                if (invoice.isPaid()) {
-                    String statusMessage = "Payment: " + "Invoice " + invoice.Number() + " - Collected amount of "
-                            + invoice.getAmount()
-                            + "\r\n";
-                    System.out.print(statusMessage);
-                    Files.write(Paths.get("paymentLog.txt"), statusMessage.getBytes(), StandardOpenOption.APPEND);
-                } else {
-                    String statusMessage = "Payment: " + "Invoice " + invoice.Number()
-                            + " - Cannot collect amount of " + invoice.getAmount()
-                            + "\r\n";
-                    System.out.print(statusMessage);
-                    Files.write(Paths.get("paymentLog.txt"), statusMessage.getBytes(), StandardOpenOption.APPEND);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+            Files.write(Paths.get("paymentLog.txt"), historyRecord.getBytes(), StandardOpenOption.APPEND);
+            if (invoice.isPaid()) {
+                String statusMessage = "Payment: " + "Invoice " + invoice.Number() + " - Collected amount of "
+                        + invoice.getAmount()
+                        + "\r\n";
+                System.out.print(statusMessage);
+                Files.write(Paths.get("paymentLog.txt"), statusMessage.getBytes(), StandardOpenOption.APPEND);
+            } else {
+                String statusMessage = "Payment: " + "Invoice " + invoice.Number()
+                        + " - Cannot collect amount of " + invoice.getAmount()
+                        + "\r\n";
+                System.out.print(statusMessage);
+                Files.write(Paths.get("paymentLog.txt"), statusMessage.getBytes(), StandardOpenOption.APPEND);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return invoice;
+    }
 
-            return invoice;
+    private Invoice payByPayPal(BigDecimal amount, String customerName, String customerEmail, String paypalPassword) {
+        Invoice invoice;
+        PayPalAccount payPalAccount = new PayPalAccount(customerEmail);
+        payPalAccount.signIn(paypalPassword);
+        boolean result = payPalAccount.deductAmount(amount);
+        invoice = new Invoice(customerName, customerEmail, amount,
+                LocalDateTime.now().toString());
+        invoice.setIsPaid(result);
 
-        } else if (payMethod == PaymentMethod.APPLE_PAY) {
-            ApplePayAccount applePayAccount = new ApplePayAccount(customerEmail, appleId);
-            applePayAccount.signIn(applePayPassword);
-            boolean result = applePayAccount.deductAmount(amount);
-            invoice = new Invoice(customerName, customerEmail, appleId, amount,
-                    LocalDateTime.now().toString());
-            invoice.setIsPaid(result);
-
-            String historyRecord = "Payment: " + "Customer " + invoice.getCustomerName()
-                    + " has made a payment with amount="
-                    + invoice.getAmount() + " on " + invoice.getPaymentDate() + " using ApplePay" + "\r\n";
-            System.out.print(historyRecord);
-            try {
-                File historyFile = new File("paymentLog.txt");
-                if (!historyFile.exists()) {
-                    historyFile.createNewFile();
-                }
-                Files.write(Paths.get("paymentLog.txt"), historyRecord.getBytes(), StandardOpenOption.APPEND);
-                if (invoice.isPaid()) {
-                    String statusMessage = "Payment: " + "Invoice " + invoice.Number() + " - Collected amount of "
-                            + invoice.getAmount()
-                            + "\r\n";
-                    System.out.print(statusMessage);
-                    Files.write(Paths.get("paymentLog.txt"), statusMessage.getBytes(), StandardOpenOption.APPEND);
-                } else {
-                    String statusMessage = "Payment: " + "Invoice " + invoice.Number()
-                            + " - Cannot collect amount of " + invoice.getAmount()
-                            + "\r\n";
-                    System.out.print(statusMessage);
-                    Files.write(Paths.get("paymentLog.txt"), statusMessage.getBytes(), StandardOpenOption.APPEND);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+        String historyRecord = "Payment: " + "Customer " + invoice.getCustomerName()
+                + " has made a payment with amount="
+                + invoice.getAmount() + " on " + invoice.getPaymentDate() + " using Paypal" + "\r\n";
+        System.out.print(historyRecord);
+        try {
+            File historyFile = new File("paymentLog.txt");
+            if (!historyFile.exists()) {
+                historyFile.createNewFile();
             }
+            Files.write(Paths.get("paymentLog.txt"), historyRecord.getBytes(), StandardOpenOption.APPEND);
+            if (invoice.isPaid()) {
+                String statusMessage = "Payment: " + "Invoice " + invoice.Number() + " - Collected amount of "
+                        + invoice.getAmount()
+                        + "\r\n";
+                System.out.print(statusMessage);
+                Files.write(Paths.get("paymentLog.txt"), statusMessage.getBytes(), StandardOpenOption.APPEND);
+            } else {
+                String statusMessage = "Payment: " + "Invoice " + invoice.Number()
+                        + " - Cannot collect amount of " + invoice.getAmount()
+                        + "\r\n";
+                System.out.print(statusMessage);
+                Files.write(Paths.get("paymentLog.txt"), statusMessage.getBytes(), StandardOpenOption.APPEND);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return invoice;
+    }
 
-            return invoice;
+    private Invoice payByCreditCard(BigDecimal amount, String customerName, String cardNumber, String cardDate,
+            String cardCvv) {
+        Invoice invoice;
+        CreditCard creditCard = new CreditCard(cardNumber, cardDate, cardCvv);
+        boolean result = creditCard.deductAmount(amount);
+        invoice = new Invoice(customerName, amount, creditCard.getLastForDigits(),
+                LocalDateTime.now().toString());
+        invoice.setIsPaid(result);
+        String historyRecord = "Payment: " + "Customer " + invoice.getCustomerName()
+                + " has made a payment with amount="
+                + invoice.getAmount() + " on " + invoice.getPaymentDate() + " using credit card" + "\r\n";
+        System.out.print(historyRecord);
+        try {
+            File historyFile = new File("paymentLog.txt");
+            if (!historyFile.exists()) {
+                historyFile.createNewFile();
+            }
+            Files.write(Paths.get("paymentLog.txt"), historyRecord.getBytes(), StandardOpenOption.APPEND);
+            if (invoice.isPaid()) {
+                String statusMessage = "Payment: " + "Invoice " + invoice.Number() + " - Collected amount of "
+                        + invoice.getAmount()
+                        + "\r\n";
+                System.out.print(statusMessage);
+                Files.write(Paths.get("paymentLog.txt"), statusMessage.getBytes(), StandardOpenOption.APPEND);
+            } else {
+                String statusMessage = "Payment: " + "Invoice " + invoice.Number()
+                        + " - Cannot collect amount of " + invoice.getAmount()
+                        + "\r\n";
+                System.out.print(statusMessage);
+                Files.write(Paths.get("paymentLog.txt"), statusMessage.getBytes(), StandardOpenOption.APPEND);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return invoice;
     }
